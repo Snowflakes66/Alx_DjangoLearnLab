c304deef6f6515dc1539104a0365568ed6c97a1d
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-02-25 11:34:40 -0800

    Created urls.py file for relationship_app

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -9,3 +9,12 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
+]
+
+
