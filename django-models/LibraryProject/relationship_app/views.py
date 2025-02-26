d7c9547449c32fc4016f89e0e25ab1f2a976e320
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-02-26 02:29:04 -0800

    Updated list_books view to correctly render template

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Book
 
+
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 from django.views.generic import DetailView
 from .models import Library
@@ -13,6 +15,8 @@ from .models import Library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
+
+
 
 
 
