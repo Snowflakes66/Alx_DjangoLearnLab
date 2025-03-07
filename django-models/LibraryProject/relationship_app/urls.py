79aadaa8be094e4ef297eb84d900e83a0d817c51
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-03-07 03:14:00 -0800

    Added correct lines of codes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -25,10 +25,11 @@ urlpatterns = [
     # Member view URL
     path('member_view/', views.member_view, name='member_view'),
 
-    path('add/', views.add_book, name='add_book'),
+    path('books/add/', views.add_book, name='add_book'),
 
     path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
-    
+
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+
 ]
 
