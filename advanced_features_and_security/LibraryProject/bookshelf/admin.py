7065da1e503a70e686398712f88bf830b1664a6c
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-03-11 17:38:31 -0700

    Fixed error

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Book
 from django.contrib.auth.admin import UserAdmin
-from .models import customUser
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
@@ -12,10 +12,10 @@ admin.site.register(Book, BookAdmin)
 
 
 class CustomUserAdmin(UserAdmin):
-    model = customUser
+    model = CustomUser
     list_display = ('username', 'email', 'date_of_birth')
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
 
-admin.site.register(customUser, CustomUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
