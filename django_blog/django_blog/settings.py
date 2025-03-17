f10e21255e6f6669b29c5d8833fd00728ffeb08a
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-03-17 15:22:42 -0700

    Configured settings.py to contain postgresql databases

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
