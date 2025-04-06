d17929bac2d91bfe7f2684109a85b66332e58a50
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-04-05 22:41:49 -0700

    Added a route in posts/urls.py for the feed endpoint

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path, include
 from rest_framework import routers
-from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView
+from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView, FeedView
+from .import views
 
 
 router = routers.DefaultRouter()
@@ -11,5 +12,7 @@ router.register(r'comments', CommentViewSet, basename='comments')
 urlpatterns = [
     path('', include(router.urls)),
     path('posts/<int:pk>/like/', LikePostView.as_view()),
-    path('posts/<int:pk>/unlike/', UnlikePostView.as_view())
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view()),
+    path('feed/', views.FeedView.as_view()),
+
 ]
