807cdc94093d19886cbcafeb2f72f3b93c66494d
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-04-05 22:53:14 -0700

    Created views to handle liking and unliking posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,18 +1,12 @@
 from rest_framework import viewsets, permissions, filters
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment, Like
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 from rest_framework.pagination import PageNumberPagination
-from .models import Post, Like
-from .serializers import PostSerializer, LikeSerializer
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from notifications.models import Notification
-
-
-
-
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
@@ -26,7 +20,6 @@ class PostViewSet(viewsets.ModelViewSet):
     pagination_class = StandardResultsSetPagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']
-
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -52,6 +45,7 @@ class LikePostView(APIView):
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         if not created:
             return Response({"message": "You already liked this post"}, status=status.HTTP_400_BAD_REQUEST)
+        Notification.objects.create(actor=request.user, recipient=post.author, verb='liked', target=post)
         return Response(LikeSerializer(like).data, status=status.HTTP_201_CREATED)
 
 class UnlikePostView(APIView):
@@ -64,14 +58,12 @@ class UnlikePostView(APIView):
         except Like.DoesNotExist:
             return Response({"message": "You didn't like this post"}, status=status.HTTP_400_BAD_REQUEST)
 
-
 class FeedView(APIView):
     def get(self, request):
         following_users = request.user.following.all()
         posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
-
 
 class LikeView(APIView):
     def post(self, request, pk):
